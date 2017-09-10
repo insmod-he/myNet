7abbdf9e30f78db8aaee19a73a001b3aefcaca17
HongliangHe <hehongliang168168@126.com>
2017-09-10 22:45:55 +0800

    1.MLP在训练集上能得到不错的结果(没有看测试集) 2.MNIST数据解析

--- layer.py
+++ layer.py
@@ -3,6 +3,7 @@ import numpy as np
 import copy
 import matplotlib.pyplot as plt
 import pdb
+import cPickle as PKL
 
 # class Layer():
 #     def __init__():
@@ -36,24 +37,39 @@ class MnistDataLayer():
         self.index_ = 0
         self.DEBUG_ = False
         
-        N = 100 # number of points per class
-        D = 2   # dimensionality
-        K = 3   # number of classes
-        X = np.zeros((N*K,D)) # data matrix (each row = single example)
-        y = np.zeros(N*K, dtype='uint8') # class labels
-        
-        for j in xrange(K):
-            ix = range(N*j,N*(j+1))
-            r = np.linspace(0.0,1,N) # radius
-            t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
-            X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
-            y[ix] = j
-        
-        if self.DEBUG_:
-            # lets visualize the data:
-            plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
-            plt.show()
-            #pdb.set_trace()
+        if not params.has_key("path"):
+            N = 100 # number of points per class
+            D = 2   # dimensionality
+            K = 3   # number of classes
+            X = np.zeros((N*K,D)) # data matrix (each row = single example)
+            y = np.zeros(N*K, dtype='uint8') # class labels
+            
+            for j in xrange(K):
+                ix = range(N*j,N*(j+1))
+                r = np.linspace(0.0,1,N) # radius
+                t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
+                X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
+                y[ix] = j
+            
+            if self.DEBUG_:
+                # lets visualize the data:
+                plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+                plt.show()
+                #pdb.set_trace()
+        else:
+            pdb.set_trace()
+            fd = open(params["path"], "r")
+            R  = PKL.load(fd)
+            fd.close()
+            train_data = R[0][0]
+            train_lab  = R[0][1]
+            val_data   = R[1][0]
+            val_lab    = R[1][1]
+            test_data  = R[2][0]
+            test_lab   = R[2][1]
+
+            X = np.array(train_data)
+            y = np.array(train_lab)
         
         self.data_X_ = X
         self.data_y_ = y
--- make_net.py
+++ make_net.py
@@ -36,15 +36,16 @@ def make_2layer_mlp():
     params = []
     
     layer1 = {}
-    layer1["batch"]  = 200
+    layer1["batch"]  = 128
     layer1["name"]   = "DATA"
     layer1["type"]   = "MnistDataLayer"
     layer1["bottom"] = []
     layer1["top"]    = ["data", "label"]
+    layer1["path"]   = "./data/mnist.pkl"
     params.append(layer1)
 
     layer2 = {}
-    layer2["output"] = 100 
+    layer2["output"] = 256 
     layer2["name"] = "FC1"
     layer2["type"] = "FCLayer"
     layer2["bottom"] = ["data"]
@@ -59,7 +60,7 @@ def make_2layer_mlp():
     params.append(layer3)
 
     layer4 = {}
-    layer4["output"] = 3      # class num
+    layer4["output"] = 10      # class num
     layer4["name"] = "FC2"
     layer4["type"] = "FCLayer"
     layer4["bottom"] = ["relu1"]
@@ -67,7 +68,69 @@ def make_2layer_mlp():
     params.append(layer4)
 
     layer5 = {}
-    layer5["class_num"] = 3
+    layer5["class_num"] = 10
+    layer5["name"]   = "SOFTMAXLOSS"
+    layer5["type"]   = "SoftmaxLossLayer"
+    layer5["bottom"] = ["fc2", "label"]
+    layer5["top"]    = ["loss"]
+    params.append(layer5)
+
+    net = Net()
+    net.init(params)
+    return net
+
+def make_3layer_mlp():
+    params = []
+    
+    layer1 = {}
+    layer1["batch"]  = 128
+    layer1["name"]   = "DATA"
+    layer1["type"]   = "MnistDataLayer"
+    layer1["bottom"] = []
+    layer1["top"]    = ["data", "label"]
+    layer1["path"]   = "./data/mnist.pkl"
+    params.append(layer1)
+
+    layer2 = {}
+    layer2["output"] = 256 
+    layer2["name"] = "FC1"
+    layer2["type"] = "FCLayer"
+    layer2["bottom"] = ["data"]
+    layer2["top"]    = ["fc1"]
+    params.append(layer2)
+
+    layer3 = {}
+    layer3["name"] = "RELU1"
+    layer3["type"] = "ReLULayer"
+    layer3["bottom"] = ["fc1"]
+    layer3["top"]    = ["relu1"]
+    params.append(layer3)
+
+    fc2_param = {}
+    fc2_param["output"] = 256 
+    fc2_param["name"] = "FC2"
+    fc2_param["type"] = "FCLayer"
+    fc2_param["bottom"] = ["relu1"]
+    fc2_param["top"]    = ["fc22"]
+    params.append(fc2_param)
+
+    relu2_param = {}
+    relu2_param["name"] = "RELU2"
+    relu2_param["type"] = "ReLULayer"
+    relu2_param["bottom"] = ["fc22"]
+    relu2_param["top"]    = ["relu2"]
+    params.append(relu2_param)
+
+    layer4 = {}
+    layer4["output"] = 10      # class num
+    layer4["name"] = "FC2"
+    layer4["type"] = "FCLayer"
+    layer4["bottom"] = ["relu2"]
+    layer4["top"]    = ["fc2"]
+    params.append(layer4)
+
+    layer5 = {}
+    layer5["class_num"] = 10
     layer5["name"]   = "SOFTMAXLOSS"
     layer5["type"]   = "SoftmaxLossLayer"
     layer5["bottom"] = ["fc2", "label"]
